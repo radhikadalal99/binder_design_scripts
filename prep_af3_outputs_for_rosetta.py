7e62c88fdb23199154251f4363b1191e0859e72b
Radhika <53919596+radhikadalal99@users.noreply.github.com>
2024-10-22 16:44:22 -0700

    Update prep_af3_outputs_for_rosetta.py

--- prep_af3_outputs_for_rosetta.py
+++ prep_af3_outputs_for_rosetta.py
@@ -4,6 +4,7 @@
 from pyrosetta import *
 init()
 import sys
+import re
 
 def get_chains_A_B(pose)
   dc=pyrosetta.rosetta.protocols.simple_moves.DeleteChainMover()
@@ -18,9 +19,16 @@ def get_rmsd(pose, refpose,name)
       pose.dump_pdb(f"{name}.pdb")
   return rmsd
 def main()
-
   input_file=sys.argv[1]
-  ref_file=sys.argv[2]
+  term=re.search("(\d+)_model, input_file)
+  if term:
+      path = glob.glob(f'/wynton/scratch/rdalal/colabfold_w_templates/cif_desns/*{term.group(1)}*.cif')
+      if path:
+          ref_file = path[0]
+      else:
+          print("No matching .cif file found.")
+  else:
+      print("Regex did not match.")
   pose=pose_from_file(input_file)
   refpose=pose_from_pdb(ref_file)
   get_chains_A_B(pose,name)
